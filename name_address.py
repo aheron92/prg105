06f4b466c53c7589f68fc4a22fb1baff2e644b63
aheron92 <70035590+aheron92@users.noreply.github.com>
2020-11-09 17:57:01 -0600

    Add files via upload

---
+++ name_address.py
@@ -0,0 +1,41 @@
+"""Name and address GUI CRUD"""
+import tkinter
+
+
+class MyGUI:
+    def __init__(self):
+        self.main_window = tkinter.Tk()
+
+        self.name_value = tkinter.StringVar()
+        self.street_value = tkinter.StringVar()
+        self.csz_value = tkinter.StringVar()
+
+        self.info_frame = tkinter.Frame(self.main_window)
+        self.button_frame = tkinter.Frame(self.main_window)
+
+        self.name_label = tkinter.Label(self.info_frame, textvariable=self.name_value)
+        self.street_label = tkinter.Label(self.info_frame, textvariable=self.street_value)
+        self.csz_label = tkinter.Label(self.info_frame, textvariable=self.csz_value)
+
+        self.name_label.pack()
+        self.street_label.pack()
+        self.csz_label.pack()
+
+        self.show_info_button = tkinter.Button(self.button_frame, text='Show Info', command=self.show)
+        self.quit_button = tkinter.Button(self.button_frame, text='Quit', command=self.main_window.destroy)
+
+        self.show_info_button.pack(side='left')
+        self.quit_button.pack(side='right')
+
+        self.info_frame.pack()
+        self.button_frame.pack()
+
+        tkinter.mainloop()
+
+    def show(self):
+        self.name_value.set('Adam Heron')
+        self.street_value.set('8900 US-14')
+        self.csz_value.set('Crystal Lake, IL 60012')
+
+
+my_gui = MyGUI()
